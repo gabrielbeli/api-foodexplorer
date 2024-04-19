aacdd8e2e52a273560519c41bce5e253e35630d7
gabriel <gabrielimabelo@gmail.com>
2024-04-20 00:13:25 +0100

    correção para upload

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 require("express-async-errors");
+require("dotenv/config");
 
-const AppError = require("./utils/AppError")
+const AppError = require("./utils/AppError");
+const uploadConfig = require('./configs/upload');
 
 const express = require("express");
 const cors = require("cors");
@@ -11,6 +13,7 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
+app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(( error, request, response, next ) => {
   if(error instanceof AppError) {
     return response.status(error.statusCode).json({
